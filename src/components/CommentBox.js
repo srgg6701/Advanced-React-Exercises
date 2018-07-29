339da0c1a507d0a94c04101c28bbc94b4b4f7471
Klevtsov <sergei.klevtsov@accenture.com>
2018-07-29 14:25:35 +0300

    Fixing bugs, start using beforeEach, start using textarea

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,3 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <div>Component box here</div>;
+export default class CommentBox extends Component {
+    /* constructor(){
+        super();
+        this.handleTextarea = this.handleTextarea.bind(this);
+    } */
+    state = {
+        comment: ''
+    }
+    handleTextarea = event => {
+        return this.setState({ 'comment' : event.target.value});
+    }
+    render() {
+        return (
+            <form>
+                <h4>Hello. Put comment here</h4>
+                <textarea onChange={this.handleTextarea} value={ this.state.comment } />
+                <div>
+                    <button>Submit it</button>
+                </div>
+            </form>
+        )
+    }
+}
--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+    wrapped = shallow(<App />);
+});
 
 it('shows the comment box', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 it('shows the comment list', () => {
-    const wrapped = shallow(<App />);
     expect(wrapped.find(CommentList).length).toEqual(1);
 });
