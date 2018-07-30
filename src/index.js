ab4c6520d4aa7087dc03da41c0f6b8147ce13f48
Klevtsov <sergei.klevtsov@accenture.com>
2018-07-30 23:31:17 +0300

    Getting understanding 'state' and 'this'

--- src/index.js
+++ src/index.js
@@ -1,36 +1,82 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import App from './app'
-class Clock extends React.Component {
+// import App from './app'
+/* class Clock extends React.Component {
     constructor() {
-      super();
-      this.state = {date: new Date()};
+        super();
+        this.state = { 
+            date: new Date(),
+            myname: 'Bob'
+        };
+        this.giveItToMe = this.giveItToMe.bind(this);
     }
-  
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    sayIt(){
+        console.log('WTF? this=>', this);
+    }
+
+    sayItAgain = () => {
+        this.setState({
+            myname: 'Wolf'
+        })
+        console.log('LOL, this=>', {this:this, state: this.state});
+    }
+
+    giveItToMe(){
+        console.log('%cthis=>', 'background: violet', this);
+    }
+
     render() {
-      return (
-        <div>
-          <h1>Hello, world!</h1>
-          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <button onClick={this.sayIt}>Say it!</button> 
+                + <span>state: {this.state.myname}</span> + 
+                <button onClick={this.sayItAgain}>sayItAgain!</button>
+                <hr/>
+                <button onClick={this.giveItToMe}>giveItToMe!</button>
+            </div>
+        );
     }
-  }
-  
-  ReactDOM.render(
+}
+
+ReactDOM.render(
     <Clock />,
     document.getElementById('root')
-  );
-/* class App extends React.Component {
+); */
+
+class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             name: 'Application'
-        }
+        };
+        this.showState = this.showState.bind(this);
     }
     
     showState(){
-        console.log('Hello, this is=>', this);
+        this.setState({
+            name: 'Nastty'
+        });
+        console.log('Hello, this name is=>', this.state.name);
     }
 
     render() {
@@ -38,7 +84,9 @@ class Clock extends React.Component {
             <div>
                 <h4>Me, at last</h4>
                 <p>{this.state.name}</p>
-                <button onClick={this.showState} >Click me</button>
+                <button onClick={this.showState}>Click me</button>
+                <hr/>
+                See state: {this.state.name}
             </div>
         );
     }
@@ -47,4 +95,4 @@ class Clock extends React.Component {
 
 ReactDOM.render(
     <App />, document.getElementById('root')
-); */
+);
