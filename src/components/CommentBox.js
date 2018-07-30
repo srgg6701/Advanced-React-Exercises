36d466dbb50f899a3b725cbb06ff46077920532a
Klevtsov <sergei.klevtsov@accenture.com>
2018-07-30 19:43:16 +0300

    Commenting for explanation

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -14,9 +14,14 @@ class CommentBox extends Component {
     handleTextarea = event => {
         return this.setState({ 'comment' : event.target.value});
     }
+    /**
+     * It stores comments. The method saveComment comes from Root (Root.js).
+     * It is defined in actions/index.js and is imported to here (see imports above)
+     * Note that saveComment gets a property of this via *connect*
+     */
     handleSubmit = event => {
         event.preventDefault();
-        // store comes from above, see the chain startinf from Root.
+        // ACTION creator; store comment 
         this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     }
@@ -32,5 +37,9 @@ class CommentBox extends Component {
         )
     }
 }
-
+// connect to actions/index.js
+// the first argument is used if we have to map store states
+// we don't need it here as it just send comments regardless of the store state
+// the second argument dispateches store (?)
+// the explanation is here: https://github.com/reduxjs/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options 
 export default connect (null, actions)(CommentBox);
