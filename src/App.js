3eb6280b099981dc1d3a0232c96661d40133f8c6
Klevtsov <sergei.klevtsov@accenture.com>
2018-08-02 06:20:16 +0300

    Experimenting with JSX-rendering and data types (Object, Array)

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello, boy! How are you?</div>
+      <div className="App">
+        <h2>Hello, boy!</h2>
+        <p>How are you?</p>
+        <hr/>
+        {<LocalGroup />}
+        <hr/>
+        {[
+          <div>Div here</div>,
+          <div>Another Div here</div>,
+          <div>And third one here</div>
+        ]}
+        <hr/>
+        {
+          {name:'someone'}
+        }
+      </div>
     );
   }
 }
+
+const LocalGroup = () => <div>Here we are!</div>
 
 export default App;
