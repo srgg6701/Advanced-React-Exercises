04fab1be10c98b831cbb9dc3cff9c9fb11954735
Klevtsov <sergei.klevtsov@accenture.com>
2018-08-11 21:24:28 +0300

    Setting auth, fixing bugs

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types'
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types'
 
 export function saveComment(comment) {
     return {
         type: SAVE_COMMENT,
         payload: comment
+    }
+};
+
+export function changeAuth(isLoggedIn){
+    return {
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
     }
 };
 
@@ -14,4 +21,4 @@ export function fetchComments(){
         type: FETCH_COMMENTS,
         payload: response
     }
-}
+};
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { Route } from 'react-router-dom'
-import CommentBox from 'components/CommentBox';
-import CommentList from 'components/CommentList';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+import * as actions from 'actions'
 
-export default () => {
-    return (
-        <React.Fragment>
-            <h1>Hello again!</h1>
-            <Route path="/post" component={CommentBox} />
-            <Route path="/" exact component={CommentList} />
-        </React.Fragment>
-    );
+class App extends Component {
+    renderButton(){
+        if(this.props.auth){
+            return <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+        } else {
+            return <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+        }
+    }
+
+    renderHeader(){
+        return (
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/post">Leave a comment</Link></li>
+                <li>{ this.renderButton() }</li>
+            </ul>
+        );
+    }
+    render(){
+        return (
+            <React.Fragment>
+                { this.renderHeader() }
+                <Route path="/post" component={CommentBox} />
+                <Route path="/" exact component={CommentList} />
+            </React.Fragment>
+        );
+    }
 }
+
+function mapStateToProps(state){
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps, actions)(App)
