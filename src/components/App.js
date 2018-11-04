af8d933db3f5375f8b3cdba4adc8b7cda736c4f6
Klevtsov <sergei.klevtsov@accenture.com>
2018-11-04 14:20:58 +0200

    Making Links and Routes dynamic

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import CommentBox from "./CommentBox";
 import CommentList from './CommentList';
+
+const LINKS = [
+    ['box', 'Comment box', CommentBox],
+    ['list', 'Comment list', CommentList]
+];
+
+function setLinksData(callback) {
+    return LINKS.map(linkData => callback(linkData));
+}
 
 export default class App extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div>Hello again!</div>
-                <Route path="/box" component={CommentBox} />
-                <Route path="/list" component={CommentList} />
+                <nav>
+                    {setLinksData(linkData => <Link to={linkData[0]}>{linkData[1]}</Link>)}
+                </nav>
+                <hr />
+                <h3>Hello again!</h3>
+                {setLinksData(linkData => <Route path={'/'+linkData[0]} component={linkData[2]} />)}
             </React.Fragment>
         )
     }
